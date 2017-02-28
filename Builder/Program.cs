29f348965dc3c1dc2481977fc1033a36be4bc7a0
Emmanuel Debanne <e.debanne@criteo.com>
2017-02-28 17:33:21 +0100

    Build in parallel

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -3,36 +3,77 @@ using System.Collections.Generic;
 using Microsoft.Build.Evaluation;
 using Microsoft.Build.Execution;
 using Microsoft.Build.Framework;
+using System.Threading;
 
 namespace Builder
 {
-    class Program
+    class Builder
     {
-        private static void MSBuild(string projectFullPath, string target)
+        private BuildManager _buildManager = BuildManager.DefaultBuildManager;
+        private Dictionary<string, string> _buildProperties = new Dictionary<string, string>();
+
+        BuildParameters _buildParameters = new BuildParameters(ProjectCollection.GlobalProjectCollection)
         {
-            Console.WriteLine("Building {0}...", projectFullPath);
+            Loggers = new[] { new SimpleConsoleLogger { Verbosity = LoggerVerbosity.Normal } },
+            MaxNodeCount = Environment.ProcessorCount
+        };
+        string _toolsVersion = "15.0";
 
-            String toolsVersion = "15.0";
-            Dictionary<string, string> buildProperties = new Dictionary<string, string>();
-            ProjectInstance projectInstance = new ProjectInstance(projectFullPath, buildProperties, toolsVersion);
-            BuildRequestData request = new BuildRequestData(projectInstance, new[] { target });
-            var buildParameters = new BuildParameters(ProjectCollection.GlobalProjectCollection)
-            {
-                Loggers = new[] { new SimpleConsoleLogger {Verbosity = LoggerVerbosity.Normal} },
-            };
-            BuildManager buildManager = BuildManager.DefaultBuildManager;
-            BuildResult buildResult = buildManager.Build(buildParameters, request);
-
-            Console.WriteLine("==> {0}", buildResult.OverallResult);
+        private BuildRequestData CreateRequest(string projectFullPath, string target)
+        {
+            ProjectInstance projectInstance = new ProjectInstance(projectFullPath, _buildProperties, _toolsVersion);
+            return new BuildRequestData(projectInstance, new[] { target });
         }
 
+        public void Build(string[] projectsFullPath, string target, bool parallelBuild)
+        {
+            if (!parallelBuild)
+            {
+                foreach (string projectFullPath in projectsFullPath)
+                {
+                    Console.WriteLine("Building {0}...", projectFullPath);
+                    BuildResult buildResult = _buildManager.Build(_buildParameters, CreateRequest(projectFullPath, target));
+                    Console.WriteLine("==> [{0}] {1}", buildResult.OverallResult, projectFullPath);
+                }
+            }
+            else
+            {
+                _buildManager.BeginBuild(_buildParameters);
+                using (CountdownEvent countdownEvent = new CountdownEvent(projectsFullPath.Length))
+                {
+                    foreach (string projectFullPath in projectsFullPath)
+                    {
+                        Console.WriteLine("Building {0} in parallel...", projectFullPath);
+                        BuildSubmission submission = _buildManager.PendBuildRequest(CreateRequest(projectFullPath, target));
+                        submission.ExecuteAsync(o => {
+                            Console.WriteLine("=========================> [{0}] {1}", o.BuildResult.OverallResult, projectFullPath);
+                            countdownEvent.Signal();
+                        }, null);
+                    }
+                    countdownEvent.Wait();
+                }
+                _buildManager.EndBuild();
+            }
+        }
+    }
+
+    class Program
+    {
         static void Main(string[] args)
         {
-            MSBuild("resources/project2/FrameworkApp.csproj", "Build");
+            Builder builder = new Builder();
 
-            Environment.SetEnvironmentVariable("MSBuildSDKsPath", @"C:\Program Files\dotnet\sdk\1.0.0-rc4-004771\Sdks");
-            MSBuild("resources/project1/CoreApp.csproj", "Restore");
-            MSBuild("resources/project1/CoreApp.csproj", "Build");
+            builder.Build(new[] { "resources/project2/FrameworkApp.csproj" }, "Build", false);
+
+            builder.Build(new[] { "resources/project1/CoreApp.csproj" }, "Restore", false);
+            builder.Build(new[] { "resources/project1/CoreApp.csproj" }, "Build", false);
+
+            Console.WriteLine("========================================");
+
+            builder.Build(new[] {
+                "resources/project1/CoreApp.csproj",
+                "resources/project2/FrameworkApp.csproj",
+            }, "Build", true);
         }
     }
 }
