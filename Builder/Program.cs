652091d01a540f41d5a1ccfe78e5846a979c362a
Emmanuel Debanne <e.debanne@criteo.com>
2017-03-02 14:59:44 +0100

    Reproduce BuildAbortedException
    This commit also fixes the issues with relative paths: MSBuild
    requires absolute paths.

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using Microsoft.Build.Evaluation;
 using Microsoft.Build.Execution;
 using Microsoft.Build.Framework;
 using System.Threading;
+using Microsoft.Build.Utilities;
 
 namespace Builder
 {
@@ -11,42 +13,52 @@ namespace Builder
     {
         private BuildManager _buildManager = BuildManager.DefaultBuildManager;
         private Dictionary<string, string> _buildProperties = new Dictionary<string, string>();
+        private Logger _logger = new SimpleConsoleLogger {Verbosity = LoggerVerbosity.Normal};
+        private string _toolsVersion = "15.0";
+        private string _resourcesAbsolutePath;
 
-        BuildParameters _buildParameters = new BuildParameters(ProjectCollection.GlobalProjectCollection)
+        public Builder(string resourcesAbsolutePath)
         {
-            Loggers = new[] { new SimpleConsoleLogger { Verbosity = LoggerVerbosity.Normal } },
-            MaxNodeCount = Environment.ProcessorCount
-        };
-        string _toolsVersion = "15.0";
+            _resourcesAbsolutePath = resourcesAbsolutePath;
+        }
 
-        private BuildRequestData CreateRequest(string projectFullPath, string target)
+        private BuildRequestData CreateRequest(string projectPath, string target)
         {
+            // The current directory is modified by MSBuild when building a project, so the absolute path must be used.
+            string projectFullPath = Path.Combine(_resourcesAbsolutePath, projectPath);
             ProjectInstance projectInstance = new ProjectInstance(projectFullPath, _buildProperties, _toolsVersion);
             return new BuildRequestData(projectInstance, new[] { target });
         }
 
-        public void Build(string[] projectsFullPath, string target, bool parallelBuild)
+        public void Build(string[] projectPaths, string target, bool parallelBuild, int maxNodeCount = 1)
         {
+            Console.WriteLine("========================================");
+
+            BuildParameters buildParameters = new BuildParameters(ProjectCollection.GlobalProjectCollection)
+            {
+                Loggers = new[] { _logger },
+                MaxNodeCount = maxNodeCount
+            };
             if (!parallelBuild)
             {
-                foreach (string projectFullPath in projectsFullPath)
+                foreach (string projectPath in projectPaths)
                 {
-                    Console.WriteLine("Building {0}...", projectFullPath);
-                    BuildResult buildResult = _buildManager.Build(_buildParameters, CreateRequest(projectFullPath, target));
-                    Console.WriteLine("==> [{0}] {1}", buildResult.OverallResult, projectFullPath);
+                    Console.WriteLine("Building {0}...", projectPath);
+                    BuildResult buildResult = _buildManager.Build(buildParameters, CreateRequest(projectPath, target));
+                    Console.WriteLine("=====> [{0}] {1}", buildResult.OverallResult, projectPath);
                 }
             }
             else
             {
-                _buildManager.BeginBuild(_buildParameters);
-                using (CountdownEvent countdownEvent = new CountdownEvent(projectsFullPath.Length))
+                _buildManager.BeginBuild(buildParameters);
+                using (CountdownEvent countdownEvent = new CountdownEvent(projectPaths.Length))
                 {
-                    foreach (string projectFullPath in projectsFullPath)
+                    foreach (string projectPath in projectPaths)
                     {
-                        Console.WriteLine("Building {0} in parallel...", projectFullPath);
-                        BuildSubmission submission = _buildManager.PendBuildRequest(CreateRequest(projectFullPath, target));
+                        Console.WriteLine("Building {0} in parallel...", projectPath);
+                        BuildSubmission submission = _buildManager.PendBuildRequest(CreateRequest(projectPath, target));
                         submission.ExecuteAsync(o => {
-                            Console.WriteLine("=========================> [{0}] {1}", o.BuildResult.OverallResult, projectFullPath);
+                            Console.WriteLine("=====> [{0}] {1}", o.BuildResult.OverallResult, projectPath);
                             countdownEvent.Signal();
                         }, null);
                     }
@@ -61,19 +73,30 @@ namespace Builder
     {
         static void Main(string[] args)
         {
-            Builder builder = new Builder();
-
-            builder.Build(new[] { "resources/project2/FrameworkApp.csproj" }, "Build", false);
-
-            builder.Build(new[] { "resources/project1/CoreApp.csproj" }, "Restore", false);
-            builder.Build(new[] { "resources/project1/CoreApp.csproj" }, "Build", false);
-
-            Console.WriteLine("========================================");
+            Builder builder = new Builder(Directory.GetCurrentDirectory());
 
             builder.Build(new[] {
-                "resources/project1/CoreApp.csproj",
-                "resources/project2/FrameworkApp.csproj",
-            }, "Build", true);
+                "resources/repo3/A/A.csproj",
+                "resources/repo3/A.UTest/A.UTest.csproj",
+                "resources/repo3/B/B.csproj",
+            }, "Clean", true, 1);
+
+            builder.Build(new[] {
+                "resources/repo3/A/A.csproj",
+                "resources/repo3/A.UTest/A.UTest.csproj",
+            }, "Clean", true, Environment.ProcessorCount);
+
+            // Failing with:
+            //   Exception non gérée: Microsoft.Build.Exceptions.BuildAbortedException: Build was canceled.Failed to successfully launch or connect to a child MSBuild.exe process.Verify that the MSBuild.exe "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" launches successfully, and that it is loading the same microsoft.build.dll that the launching process loaded. If the location seems incorrect, try specifying the correct location in the BuildParameters object, or with the MSBUILD_EXE_PATH environment variable.
+            //     à Microsoft.Build.Execution.BuildManager.EndBuild()
+            // Whereas it works if
+            //    <ProjectReference Include="..\A\A.csproj">
+            // is deleted in 'A.UTest.csproj'.
+            builder.Build(new[] {
+                "resources/repo3/A/A.csproj",
+                "resources/repo3/A.UTest/A.UTest.csproj",
+                "resources/repo3/B/B.csproj",
+            }, "Clean", true, Environment.ProcessorCount);
         }
     }
 }
